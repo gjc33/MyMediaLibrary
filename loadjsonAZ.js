8a6b7d516ba98290c0537516bf594078ad7169ab
gjc33 <97050192+gjc33@users.noreply.github.com>
2025-08-01 19:04:55 +0100

    Update loadjsonAZ.js

--- loadjsonAZ.js
+++ loadjsonAZ.js
@@ -13,60 +13,12 @@ fetch('AZlist.json')
 
 	LibraryTotal.innerHTML = `${Object.keys(allFilms).length} Titles`;
 
-	let t="";
 	let TabDataHere = document.querySelector(`#Tabs`);
-	
-/*
-	let Categories = allFilms.map(Cats => Cats.Category);
-	
-	var lookup = {};
-	var UniqueCat = [""];
-	for(Cat of Categories){
-		if(!(Cat in lookup)){
-				lookup[Cat] = 1;
-				UniqueCat.push(Cat);
-		}
-	}
-	UniqueCat.shift()
-	UniqueCat.sort();
-	//document.getElementById("info").innerHTML = `[${page}]`;
-
-
-
+	let t="";
 	let ckd = "checked";
 
+	
 
-
-	if(page != "A.html" && page !='' && page !="index.html"){
-		let ti = 0;
-		for(Cat of UniqueCat){
-			ti++;
-			if(ti >1){ckd = ""};
-			if(ti != 0){ t += `<input type='radio' id='tabToggle0${ti}' name='tabs' value='${ti}'  ${ckd}/><label for='tabToggle0${ti}'>${Cat}</label>`;};
-		}
-
-		for(Cat of UniqueCat){
-			let CatFilms = allFilms.filter((CatData) => {
-				if (Cat === "") {return CatData}
-				else if (CatData.Category.includes(Cat)){
-					return CatData
-				}				
-			})
-			
-			var CatTitles = Object.keys(CatFilms).length;
-					
-			if(page == 'D.html'){t += `<tab-content></br><p>[${CatTitles} Titles]</p></br><div class='table'><div class='row'>`};
-			if(page == 'M.html'){t += `<tab-content></br><p>[${CatTitles} Titles]</p></br><ul class='list'>`};
-			for(cf of CatFilms){
-				if(page == 'D.html'){t+= `<div class='cell'><a href='${cf.Desktop}${cf.MovieFile}'><img loading='lazy' src='${cf.CoverArt}'></a></br>${cf.Title}</div>`};
-				if(page == 'M.html'){t+= `<li class='item'><div class='content'><a href='${cf.Mobile}${cf.MovieFile}'><img loading='lazy' src='${cf.CoverArt}'></a></br>${cf.Title}</div></li>`};
-			}
-			if(page == 'D.html'){t += `</div></div></tab-content>`};
-			if(page == 'M.html'){t+= `</ul></tab-content>`};
-		}
-	}
-	else {
-*/
 		const Alphabet = ["#ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 
 		for(a=0; a < (Alphabet[0].length); a++){
@@ -127,7 +79,6 @@ fetch('AZlist.json')
 			}
 			//t += '</tab-content>';
 		}
-//	}
 	
 	TabDataHere.innerHTML = t;	
 	
